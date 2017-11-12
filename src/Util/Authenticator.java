138e2ca1c4ddc957e74fb2b5805a357196783278
Eduardo Silva <emf.silva@campus.fct.unl.pt>
2017-11-12 19:22:23 +0000

    Login test

--- src/Util/Authenticator.java
+++ src/Util/Authenticator.java
@@ -6,10 +6,15 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import Resources.Account;
+import Resources.AccountsBook;
 
 public class Authenticator extends HttpServlet{
+	
+	private AccountsBook accountsBook = new AccountsBook();
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
@@ -17,7 +22,22 @@ public class Authenticator extends HttpServlet{
 	}
 	
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException ,IOException {
-		
+		if(req.getParameter("loginButton") != null) {
+			try {
+				String name = req.getParameter("name");
+				String password = req.getParameter("password");
+				HttpSession session = req.getSession(true);
+				Account authUser = login(name, password);
+				//TODO session.setAttribute(JWT, authUser.getJWT());
+				resp.sendRedirect("MainMenu");
+			} catch (AuthenticationErrorException e) {
+				resp.sendRedirect("MainPage");
+			} catch (LockedAccountException e) {
+				resp.sendRedirect("MainPage");
+			} catch (UndefinedAccountException e) {
+				resp.sendRedirect("MainPage");
+			}
+		}
 	};
 	
 	/**
@@ -64,9 +84,20 @@ public class Authenticator extends HttpServlet{
 	}
 
 
-	private Account login(String name, String pwd) {
+	private Account login(String name, String pwd) throws UndefinedAccountException, LockedAccountException, AuthenticationErrorException {
+		Account account = accountsBook.getAccount(name);
+		if(account == null)
+			throw new UndefinedAccountException();
+		//TODO boolean isLocked = account.isLocked();
+		boolean isLocked = false;
+		if(isLocked)
+			throw new LockedAccountException();
+		//TODO check how to hash password
+		if(!account.getPassword().equals(pwd.hashCode()))
+			throw new AuthenticationErrorException();
+		//TODO Set locked with: account.setLocked()
 		System.out.println("login finish.");
-		return null;
+		return account;
 	}
 
 	/**
@@ -92,7 +123,7 @@ class UndefinedAccountException extends Exception{
 
 }
 
-class LocketAccountException extends Exception{
+class LockedAccountException extends Exception{
 
 }
 
