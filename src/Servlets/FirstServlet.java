b45d4595ed4f9c029d17147b09206b154b7b568d
DESKTOP-QI030P1\tomas <t.pessanha@campus.fct.unl.pt>
2017-11-12 19:42:38 +0000

    Added more stuff

---
+++ src/Servlets/FirstServlet.java
@@ -0,0 +1,39 @@
+package Servlets;
+
+import Resources.Account;
+import Resources.AccountsBook;
+import Resources.Storage;
+import Util.Constants;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FirstServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        //Response type
+        response.setContentType("text/html");
+
+        //HTML
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello World!!!</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>" + Constants.DATABASE_FILE_PATH + "</h1>");
+        out.println("</body>");
+        out.println("</html>");
+
+        //CODE
+        Account acc = new Account("test", "test");
+        AccountsBook book = new AccountsBook();
+        book.addAccount(acc);
+        Storage.SaveChangesToStorage(book);
+    }
+}
