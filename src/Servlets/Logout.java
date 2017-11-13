912c3e3f1af5340f7e9d78d8a3da5c57a47a9f13
Eduardo Silva <emf.silva@campus.fct.unl.pt>
2017-11-13 22:11:29 +0000

    Removed old commented code

--- src/Servlets/Logout.java
+++ src/Servlets/Logout.java
@@ -32,33 +32,5 @@ public class Logout extends HttpServlet{
         } catch (Exception e) {
             out.println(e.getMessage());
 		}
-        
-//		//Creating HTML page
-//		resp.setContentType("text/html");
-//		PrintWriter out = resp.getWriter();
-//		out.println("<center>");
-//		out.println("<h1>Create Account</h1>");
-//		out.println("<br>");
-//		if(req.getParameter("Message") != null)
-//			out.println(req.getParameter("Message"));
-//		
-//		out.print("Are you sure you want to logout?");
-//		out.print("<form action=\"");
-//		out.print("Authenticator\" ");
-//		out.println("method=POST>");
-//		out.println("<input type=submit name=logoutButton value='Logout'>");
-//		out.println("</form>");
-//		
-//		out.print("<form action=\"");
-//		out.print("MainMenu\" ");
-//		out.println("method=GET>");
-//		out.println("<input type=submit name=cancelLogout value='Cancel'>");
-//		out.println("</form>");
-//		
-//		//Finishing HTML page and closing writer
-//		out.println("</center>");
-//		out.println("</body>");
-//		out.println("</html>");
-//		out.close();
 	}
 }
